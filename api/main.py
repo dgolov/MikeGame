c15ea829332509e0c0bace33bb79cde8a01c2235
dgolov <dmitriy.evgenyevich@gmail.com>
2023-11-11 02:23:27 +0300

    pep8 refactoring

--- api/main.py
+++ api/main.py
@@ -23,7 +23,7 @@ async def exception_middleware(request: Request, call_next):
     try:
         response = await call_next(request)
         logger.debug(
-            f"{request.scope['client'][0]}:{request.scope['client'][1]} - '{request.method} {request.scope['path']} "
+            f"{request.scope['client'][0]}: {request.scope['client'][1]} - '{request.method} {request.scope['path']} "
             f"{request.scope['scheme']}/{request.scope['http_version']} {response.status_code}'"
         )
         return response
