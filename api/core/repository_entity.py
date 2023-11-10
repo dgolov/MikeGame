222bb3d40a615e42087c47470bcd31140ab2c3f1
dgolov <dmitriy.evgenyevich@gmail.com>
2023-11-11 02:22:39 +0300

    Updated create player repository method

--- api/core/repository_entity.py
+++ api/core/repository_entity.py
@@ -1,5 +1,40 @@
-from sqlalchemy import select, insert
+from core.engine import async_session_maker
+from datetime import datetime
 from game import models, schemas
+from sqlalchemy import select, insert
+
+
+async def init_db() -> None:
+    """ Initialisation empty db
+    :return:
+    """
+    currency_list = ["bottle", "RUB", "USD", "BTC"]
+
+    async with async_session_maker() as session:
+        need_commit = False
+
+        query = select(models.Currency)
+        try:
+            result = await session.execute(query)
+        except Exception as e:
+            print(e)
+            return
+        existing_currencies = result.all()
+
+        currencies = []
+        for item_currency_name in currency_list:
+            currencies.append(
+                models.Currency(name=item_currency_name)
+            )
+
+        if not len(existing_currencies):
+            for currency in currencies:
+                print(f"Add {currency}")
+                session.add(currency)
+                need_commit = True
+
+        if need_commit:
+            await session.commit()
 
 
 class Base:
@@ -61,8 +96,44 @@ class PlayerEntity(Base):
         result = await self.session.execute(query)
         return self._first(result)
 
-    async def create(self, data: schemas.CreatePlayer | dict) -> dict:
-        return await self._add(obj=self.model, data=data)
+    async def create(self, data: schemas.CreatePlayer | dict) -> int:
+        query = select(models.Currency)
+        result = await self.session.execute(query)
+        all_currencies = [item[0] for item in result]
+
+        if not isinstance(data, dict):
+            data = data.dict()
+
+        player = self.model(**data)
+        self.session.add(player)
+        await self.session.commit()
+
+        if all_currencies and isinstance(all_currencies, list):
+            for item_currency in all_currencies:
+                balance_name = self.__map_balance_name(currency_name=item_currency.name)
+                if not balance_name:
+                    continue
+                balance = models.Balance(
+                    name=balance_name,
+                    currency_id=item_currency.id,
+                    player_id=player.id,
+                    amount=0,
+                    updated_at=datetime.now()
+                )
+                self.session.add(balance)
+
+        await self.session.commit()
+        return player.id
+
+    @staticmethod
+    def __map_balance_name(currency_name: str) -> str | None:
+        balance_mapping = {
+            "bottle": "Бутылки",
+            "RUB": "Рубли",
+            "USD": "Баксы",
+            "BTC": "Биткоины"
+        }
+        return balance_mapping.get(currency_name, None)
 
 
 class CurrencyEntity(Base):
