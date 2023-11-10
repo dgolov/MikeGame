110ea040a6d1a79c153b7d0309b5bf47971fc720
dgolov <dmitriy.evgenyevich@gmail.com>
2023-11-11 00:20:32 +0300

    updated user model

--- api/users/models.py
+++ api/users/models.py
@@ -16,12 +16,7 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     is_active: bool = Column(Boolean, default=True, nullable=False)
     is_superuser: bool = Column(Boolean, default=False, nullable=False)
     is_verified: bool = Column(Boolean, default=False, nullable=False)
-    players: Mapped[list] = relationship(
-        "Player",
-        back_populates="user",
-        lazy="selectin",
-        join_depth=2
-    )
+    players = relationship("Player", lazy="selectin", join_depth=2)
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
